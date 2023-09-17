67408d2ee49df12bf34101208d87d7036696d140
MehrnooshZiaei <s.mehrnooshziaei@gmail.com>
2023-09-17 20:26:41 +0330

    Video 18 - Creating reusable, custom styled button

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget{
   const QuestionsScreen({super.key});
@@ -24,28 +25,35 @@ class _QuestionsScreenState extends State<QuestionsScreen>{
         mainAxisAlignment: MainAxisAlignment.center, //MainAxisAlignment.spaceAround
           children: [
             const Text(
-              'The Question',
+              'The Question...',
+              style: TextStyle(
+                color: Colors.white
+              ),
             ),
             const SizedBox(
               height: 30,
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Answer 1')
+            // ElevatedButton(
+            //   onPressed: () {},
+            //   child: const Text('Answer 1')
+            // ),
+            AnswerButton(
+              answerText: 'Answer 1',
+             onTap: (){}
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Answer 2')
+            AnswerButton(
+              answerText: 'Answer 2', 
+              onTap: (){}
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Answer 3')
+            AnswerButton(
+              answerText: 'Answer 3', 
+              onTap: (){}
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Answer 4')
+            AnswerButton(
+              answerText: 'Answer 4', 
+              onTap: (){}
             ),
-          ],
+          ]
       ),
     );
   }
