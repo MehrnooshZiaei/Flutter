6f1bd4de1f139607ee128ec8bf0a2f11af0766ec
MehrnooshZiaei <s.mehrnooshziaei@gmail.com>
2023-09-17 22:11:29 +0330

    Video 21 - Add Padding and margin

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -24,24 +24,28 @@ class _QuestionsScreenState extends State<QuestionsScreen>{
 
     return SizedBox( // Alternative to using center
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center, //MainAxisAlignment.spaceAround
+      child: Container(
+        margin: const EdgeInsets.all(40), // top, bottom, left, right
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, //MainAxisAlignment.spaceAround
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Text(
               currentQuestion.text,
               style: const TextStyle(
                 color: Colors.white
               ),
+              textAlign: TextAlign.center,
             ),
             const SizedBox(
               height: 30,
             ),
-            
+      
             // ElevatedButton(
             //   onPressed: () {},
             //   child: const Text('Answer 1')
             // ),
-            
+              
             // AnswerButton(
             //   answerText: currentQuestion.answers[0],
             //  onTap: (){}
@@ -58,11 +62,12 @@ class _QuestionsScreenState extends State<QuestionsScreen>{
             //   answerText: currentQuestion.answers[3], 
             //   onTap: (){}
             // ),
-
+      
             ...currentQuestion.answers.map((answer) {
               return AnswerButton(answerText: answer, onTap: (){});
             }),
           ]
+        ),
       ),
     );
   }
