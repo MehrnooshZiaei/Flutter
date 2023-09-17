3dd122d80fcf8f00975a00432f3802d5ff532f79
MehrnooshZiaei <s.mehrnooshziaei@gmail.com>
2023-09-17 23:44:42 +0330

    video 28 - Getting started with Results screen, building a dummy page

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/results_screen.dart';
 
 class Quiz extends StatefulWidget{
   Quiz({super.key});
@@ -38,7 +39,7 @@ class _QuizState extends State<Quiz>{
 
     if(selectedAnswers.length == questions.length){
       setState(() {
-        activeScreen = 'start-screen';
+        activeScreen = 'results-screen';
         selectedAnswers = [];
       });
     }
@@ -53,6 +54,9 @@ class _QuizState extends State<Quiz>{
     Widget screenWidget = StartScreen(switchScreen);
     if (activeScreen == 'questions-screen') {
       screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer);
+    }
+    if (activeScreen == 'results-screen'){
+      screenWidget = const ResultsScreen();
     }
 
     return MaterialApp(
