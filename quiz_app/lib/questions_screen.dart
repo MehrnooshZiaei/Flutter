53f3a87e93a00762fb9c497ba0365623fd24862a
MehrnooshZiaei <s.mehrnooshziaei@gmail.com>
2023-09-17 20:05:25 +0330

    Video 17 - Configuring Column, Basic view of Questions added

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -18,9 +18,34 @@ class _QuestionsScreenState extends State<QuestionsScreen>{
 
   @override
   Widget build(context){
-    return Center(
+    return SizedBox( // Alternative to using center
+      width: double.infinity,
       child: Column(
-
+        mainAxisAlignment: MainAxisAlignment.center, //MainAxisAlignment.spaceAround
+          children: [
+            const Text(
+              'The Question',
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Answer 1')
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Answer 2')
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Answer 3')
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Answer 4')
+            ),
+          ],
       ),
     );
   }
