a1412912fb4dfa4e02fd5fe5c4ed898cbb49e0d7
MehrnooshZiaei <s.mehrnooshziaei@gmail.com>
2023-09-18 09:27:20 +0330

    Video 31,32,33 - Designing the result page, Adding Rows and columns together

--- quiz_app/lib/results_screen.dart
+++ quiz_app/lib/results_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget{
   const ResultsScreen({super.key, required this.choosenAnswers});
@@ -13,7 +14,7 @@ class ResultsScreen extends StatelessWidget{
     for (var i = 0; i < choosenAnswers.length; i++) {
       summary.add({
         'question_index': i,
-        'question_text': questions[i].text,
+        'question': questions[i].text,
         'correct_answer': questions[i].answers[0], // The first answer is always the right one and we do not shuffle them
         'user_answer': choosenAnswers[i]
       });
@@ -37,9 +38,7 @@ class ResultsScreen extends StatelessWidget{
             const SizedBox(
               height: 30,
             ),
-            const Text(
-              'List of Answers and questions ...',
-            ),
+            QuestionsSummary(getSummaryData()),
             const SizedBox(
               height: 30,
             ),
