d12a95a19d0bf3319d2a04a5174e7abb002cb68a
MehrnooshZiaei <s.mehrnooshziaei@gmail.com>
2023-09-19 13:34:44 +0330

    Video 29 - setting text color

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -11,9 +11,28 @@ void main(){
         useMaterial3: true,
         // scaffoldBackgroundColor: const Color.fromARGB(255, 220, 189, 252),
         colorScheme: kColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
+        appBarTheme: const AppBarTheme().copyWith(
           backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer
+          foregroundColor: kColorScheme.primaryContainer, // It overwrites the titleLarge color
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16, 
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          )
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.onSecondary,
+            fontSize: 14,
+          )
         )
       ),
       home: const Expenses(),
