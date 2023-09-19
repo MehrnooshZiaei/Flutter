d57536e51ca4ab3c550ee5edcf4a544365b5a879
MehrnooshZiaei <s.mehrnooshziaei@gmail.com>
2023-09-19 13:14:57 +0330

    Video 28 - themeing

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -4,7 +4,11 @@ import 'package:expense_tracker/widgets/expenses.dart';
 void main(){
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true,),
+      // With copyWith we over write some parameters that we want to
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        // scaffoldBackgroundColor: Color.fromARGB(255, 220, 189, 252),
+      ),
       home: Expenses(),
     )
   );
