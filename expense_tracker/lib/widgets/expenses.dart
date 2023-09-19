c4fe808c046ed81d15af47ae96c728547b6ab28b
MehrnooshZiaei <s.mehrnooshziaei@gmail.com>
2023-09-19 13:00:48 +0330

    Video 27 - Showing and Managing snackbars

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -32,13 +32,37 @@ class _ExpensesState extends State<Expenses>{
   }
 
   void _removeExpense(Expense expense){
+    final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted'),
+        action: SnackBarAction(
+          label: 'Undo', 
+          onPressed: (){
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          }
+        ),
+      )
+    );
   }
 
   @override
   Widget build(BuildContext context){
+    Widget mainContent = const Center(
+      child: Text('No Expenses found. Start adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty){
+      mainContent = ExpensesList(expenses: _registeredExpenses, onRemoveExpense: _removeExpense,);
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter ExpenseTracker'),
@@ -55,7 +79,7 @@ class _ExpensesState extends State<Expenses>{
           // Way1: toolbar with the add button => Row()
           const Text("The Chart"),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses, onRemoveExpense: _removeExpense,),
+            child: mainContent,
           ),
         ],
       ),
