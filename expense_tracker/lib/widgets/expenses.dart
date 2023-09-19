1bf86e1fd2779650014f27c9fc57b0aaa7e3590e
MehrnooshZiaei <s.mehrnooshziaei@gmail.com>
2023-09-19 09:44:58 +0330

    Video 12 - Handling User Input + ShowModalButtomSheet

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 
 class Expenses extends StatefulWidget{
   const Expenses({super.key});
@@ -16,13 +17,21 @@ class _ExpensesState extends State<Expenses>{
     Expense(title: 'Cinema', amount: 15.69, date: DateTime.now(), category: Category.leisure),
   ];
 
+  void _openAddExpenseOverlay(){
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
+        title: const Text('Flutter ExpenseTracker'),
         actions: [
           IconButton(
-            onPressed: (){}, 
+            onPressed: _openAddExpenseOverlay, 
             icon: const Icon(Icons.add),
           )
         ],
