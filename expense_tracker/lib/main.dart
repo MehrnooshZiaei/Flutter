10d084eca0b7211ec7bfc524ffb0bf3e611fe665
MehrnooshZiaei <s.mehrnooshziaei@gmail.com>
2023-09-19 14:47:49 +0330

    Video 31 - Using system theme color (dark/light) mode

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -2,11 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125)
+);
 
 void main(){
   runApp(
     MaterialApp(
       // With copyWith we over write some parameters that we want to
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16, 
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer
+          )
+        ),
+      ),
       theme: ThemeData().copyWith(
         useMaterial3: true,
         // scaffoldBackgroundColor: const Color.fromARGB(255, 220, 189, 252),
@@ -35,6 +56,7 @@ void main(){
           )
         ),
       ),
+      // themeMode: ThemeMode.system, //Default
       home: const Expenses(),
     )
   );
