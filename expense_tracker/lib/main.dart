b4d8dbfc0894a30d00f723bb5c6e7003b72a3c36
MehrnooshZiaei <s.mehrnooshziaei@gmail.com>
2023-09-19 13:24:00 +0330

    Video 29 - Setting and using colorScheme, specifically for appBar

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
 void main(){
   runApp(
     MaterialApp(
       // With copyWith we over write some parameters that we want to
       theme: ThemeData().copyWith(
         useMaterial3: true,
-        // scaffoldBackgroundColor: Color.fromARGB(255, 220, 189, 252),
+        // scaffoldBackgroundColor: const Color.fromARGB(255, 220, 189, 252),
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer
+        )
       ),
-      home: Expenses(),
+      home: const Expenses(),
     )
   );
 }
