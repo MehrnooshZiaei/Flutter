9605623e05f8119b18cd229f2a8b0b64e6b74c9a
MehrnooshZiaei <s.mehrnooshziaei@gmail.com>
2023-09-19 11:59:21 +0330

    Video 22 - Validating User input

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -39,6 +39,29 @@ class _NewExpenseState extends State<NewExpense>{
     setState(() {
       _selectedDate = pickedDate;
     });
+  }
+
+  void _submitExpenseData(){
+    final enteredAmount = double.tryParse(_amountController.text); // tryParse('Hello') = null
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0; 
+    if (_titleController.text.trim().isEmpty || amountIsInvalid || _selectedDate == null ){
+      showDialog(
+        context: context, 
+        builder: (ctx) => AlertDialog(
+          title: const Text('Invalid input!'),
+          content: const Text('Please make sure a valid title, amount, date and category was entered.'),
+          actions: [
+            TextButton(
+              onPressed: (){
+                Navigator.pop(context);
+              }, 
+              child: const Text('Okay'),
+            )
+          ],
+        ),
+      );
+      return;
+    }
 
   }
 
@@ -121,10 +144,7 @@ class _NewExpenseState extends State<NewExpense>{
                 child: const Text('Cancel'),
               ),
               ElevatedButton(
-                onPressed: (){
-                  print(_titleController.text);
-                  print(_amountController.text);
-                }, 
+                onPressed: _submitExpenseData, 
                 child: const Text("Save Expense"),
               ),
             ],
