87639589fdf65d2a4cc895a6901fe70151d8d420
MehrnooshZiaei <s.mehrnooshziaei@gmail.com>
2023-09-23 00:08:38 +0330

    video 9 - Creating Adaptive apps, Here different error message for android and ios

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
@@ -41,10 +44,24 @@ class _NewExpenseState extends State<NewExpense>{
     });
   }
 
-  void _submitExpenseData(){
-    final enteredAmount = double.tryParse(_amountController.text); // tryParse('Hello') = null
-    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0; 
-    if (_titleController.text.trim().isEmpty || amountIsInvalid || _selectedDate == null ){
+  void _showDialog(){
+    if (Platform.isIOS){
+      showCupertinoDialog(
+        context: context, 
+        builder: (ctx) => CupertinoAlertDialog(
+          title: const Text('Invalid input!'),
+          content: const Text('Please make sure a valid title, amount, date and category was entered.'),
+          actions: [
+            TextButton(
+              onPressed: (){
+                Navigator.pop(context);
+              }, 
+              child: const Text('Okay'),
+            )
+          ],
+        ),
+      );
+    } else {
       showDialog(
         context: context, 
         builder: (ctx) => AlertDialog(
@@ -60,6 +77,15 @@ class _NewExpenseState extends State<NewExpense>{
           ],
         ),
       );
+    }
+      
+  }
+
+  void _submitExpenseData(){
+    final enteredAmount = double.tryParse(_amountController.text); // tryParse('Hello') = null
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0; 
+    if (_titleController.text.trim().isEmpty || amountIsInvalid || _selectedDate == null ){
+      _showDialog();
       return;
     }
     widget.onAddExpense(Expense(title: _titleController.text, amount: enteredAmount, date: _selectedDate!, category: _selectedCategory));
